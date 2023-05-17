11e1f70f94e576f1d2e149f60f25c5cd3a98465e
Mizara983 <pounasatu@gmail.com>
2023-05-17 11:19:23 +0300

    Libéllé Champ à changer

--- models/res_company.py
+++ models/res_company.py
@@ -9,5 +9,5 @@ class Company(models.Model):
     _inherit = 'res.company'
 
     x_attendance_user = fields.Many2many(string='Déstinataire du corrier(présence)', comodel_name="res.users")
-    all_the = fields.Selection(string="Semaine / Jour",selection=[('jour', 'Jour'), ('semaine', 'Semaine')], default='jours')
+    all_the = fields.Selection(string="Envoyer par",selection=[('jour', 'Jour'), ('semaine', 'Semaine')], default='jours')
 
