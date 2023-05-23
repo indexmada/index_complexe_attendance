f15379488aaee097d554b1e0691c336dffef5123
Mizara983 <pounasatu@gmail.com>
2023-05-23 09:50:56 +0300

    debug attendance jours

--- models/res_company.py
+++ models/res_company.py
@@ -9,7 +9,7 @@ class Company(models.Model):
     _inherit = 'res.company'
 
     x_attendance_user = fields.Many2many(string='Déstinataire du corrier(présence)', comodel_name="res.users")
-    all_the = fields.Selection(string="Envoyer par",selection=[('jour', 'Jour'), ('semaine', 'Semaine')], default='jours')
+    all_the = fields.Selection(string="Envoyer par",selection=[('jour', 'Jour'), ('semaine', 'Semaine')], default='jour')
 
     enable_webcam = fields.Boolean(string="Activer Webcam",default=False)
 
